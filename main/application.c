72f0e31d39d175d7ecd28a85686e6ba4092fa41f
Renze Nicolai <renze@rnplus.nl>
2024-01-16 01:51:41 +0100

    Separate function for settings menu

--- main/application.c
+++ main/application.c
@@ -234,6 +234,30 @@ unsigned long millis() {
     return (unsigned long)(esp_timer_get_time() / 1000ULL);
 }
 
+void menu_settings() {
+    pax_buf_t *gfx  = bsp_get_gfx_buffer();
+    QueueHandle_t queue = bsp_get_button_queue();
+
+    while (1) {
+        bsp_apply_lut(lut_4s);
+        pax_background(gfx, 0);
+        pax_draw_text(gfx, 1, pax_font_marker, 18, 1, 0, "R settings placeholder");
+        DisplaySwitchesBox(SWITCH_1);
+        pax_draw_text(gfx, 1, pax_font_sky_mono, 10, 8, 116, "Exit");
+        bsp_display_flush();
+
+        coprocessor_input_message_t buttonMessage = {0};
+        if (xQueueReceive(queue, &buttonMessage, portMAX_DELAY) == pdTRUE) {
+            printf("In settings: button %u state changed to %u\n", buttonMessage.button, buttonMessage.state);
+            if (buttonMessage.state == SWITCH_PRESS) {
+                if (buttonMessage.button == SWITCH_1) {
+                    break; // Exit
+                }
+            }
+        }
+    }
+}
+
 void app_thread_entry(void) {
     esp_err_t res;
 
@@ -350,7 +374,10 @@ void app_thread_entry(void) {
                         break;
                     }
                     if (MainMenuchangeflag == 1) {
-                        bsp_apply_lut(lut_4s);
+                        menu_settings();
+                        MainMenustatemachine = MainMenuhub;
+                        MainMenuchangeflag   = 1;
+                        /*bsp_apply_lut(lut_4s);
                         pax_background(gfx, 0);
                         pax_draw_text(gfx, 1, pax_font_marker, 18, 1, 0, "Settings placeholder");
                         DisplaySwitchesBox(SWITCH_1);
@@ -360,7 +387,7 @@ void app_thread_entry(void) {
                         // DisplaySwitchesBox(SWITCH_5);
                         // pax_draw_text(gfx, 1, pax_font_sky_mono, 10, 247, 116, "Offline");
                         bsp_display_flush();
-                        MainMenuchangeflag = 0;
+                        MainMenuchangeflag = 0;*/
                     }
                     break;
 
