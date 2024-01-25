250f5c7303f0a406c999ae876d6f0d349766ee77
Squirl <>
2024-01-25 21:20:02 +0100

    added docs

--- components/badge_comms/include/badge_comms.h
+++ components/badge_comms/include/badge_comms.h
@@ -16,7 +16,28 @@ typedef struct {
     uint8_t                    data_len_to_send;
 } badge_comms_message_t;
 
+/**
+ * broadcast a message to other badges around
+ * @param comms_message the message to be sent
+ */
 void badge_comms_send_message(badge_comms_message_t* comms_message);
 
+/**
+ * 'subscribe' to a message type
+ * @param message_type the message type to subscribe to
+ * @param expected_message_len the sizeof the datastruct the receiver is expecting
+ * @return a queue handle, where received messages are sent to.
+ * return NULL if something went wrong, check logs for more info
+ */
 QueueHandle_t badge_comms_add_listener(badge_comms_message_type_t message_type, uint8_t expected_message_len);
-esp_err_t     badge_comms_remove_listener(QueueHandle_t queue);
+
+/**
+ * 'unsubscribe' from a message type
+ * @param queue the queue handle received when calling badge_comms_add_listener
+ * @return
+ *      - ESP_OK on success
+ *      - ESP_ERR_INVALID_ARG when no queue was provided
+ *      - ESP_ERR_INVALID_STATE when the badge comms component has not been initialised
+ *      - ESP_ERR_NOT_FOUND if the provided queue was not found in the list of listeners
+ */
+esp_err_t badge_comms_remove_listener(QueueHandle_t queue);
